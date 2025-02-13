c20d845058c57a539cac479510e411519508b413
Miguel Ángel García López <mangel111111111@gmail.com>
2025-02-13 11:59:56 +0100

    feat: add AirplaneController

---
+++ app/Http/Controllers/Api/AirplaneController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Airplane;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AirplaneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $airplanes = Airplane::all();
+
+        return response()->json($airplanes, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'model' => 'required|string',
+            'seatCapacity' => 'required|integer',
+        ]);
+
+        $airplane = Airplane::create($validatedData);
+
+        return response()->json($airplane, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $airplanes = Airplane::find($id);
+
+        return response()->json($airplanes, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $airplanes = Airplane::find($id);
+
+        $validated = $request->validate([
+            'model' => 'required|string',
+            'seatCapacity' => 'required|integer',
+        ]);
+
+        $airplanes->update([
+            'model' => $validated['model'],
+            'seatCapacity' => $validated['seatCapacity'],
+        ]);
+
+        $airplanes->save();
+
+        return response()->json($airplanes, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $airplanes = Airplane::find($id);
+
+        $airplanes->delete();
+
+        return response()->json(['message' => 'Airplane deleted successfully'], 204);
+    }
+}
