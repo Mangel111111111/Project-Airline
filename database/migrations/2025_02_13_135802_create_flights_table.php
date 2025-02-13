5e46faa34c2a8afd1bd7f1e8e73428f6cb10bf8a
Miguel Ángel García López <mangel111111111@gmail.com>
2025-02-13 15:13:50 +0100

    feat: create flights migration

---
+++ database/migrations/2025_02_13_135802_create_flights_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->string('origin');
+            $table->string('destination');
+            $table->dateTime('departureTime');
+            $table->dateTime('arrivalTime');
+            $table->foreignId('airplane_id')->constrained()->onDelete('cascade');
+            $table->integer('availableSeats');
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flights');
+    }
+};
