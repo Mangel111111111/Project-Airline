5cbfe8ba8de3ad0caceca15ca9bd7575b2b862e0
Miguel Ángel García López <mangel111111111@gmail.com>
2025-03-28 16:14:15 +0100

    test: add user relationship test

--- tests/Feature/Models/UserTest.php
+++ tests/Feature/Models/UserTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature\Models;
 
 use Tests\TestCase;
 use App\Models\User;
+use App\Models\Flight;
+use App\Models\Reservation;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -36,5 +38,17 @@ class UserTest extends TestCase
         $user = new User();
 
         $this->assertEquals(['password', 'remember_token'], $user->getHidden());
+    }
+
+    public function test_CheckIfUserHasManyReservations(): void
+    {
+        $user = User::factory()->create();
+
+        Reservation::factory()->count(3)->create([
+            'user_id' => $user->id,
+        ]);
+
+        $this->assertCount(3, $user->reservations);
+        $this->assertInstanceOf(Reservation::class, $user->reservations->first());
     }
 }
